d1008d250c1a56d67d6438ed04da7a3b195d2e0b
msalsas <manolez@gmail.com>
2019-07-23 12:57:22 +0200

    Add JMSSerializerBundle and FOSRestBundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
