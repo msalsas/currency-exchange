25a26e838b152eff3b2fffe3ace3f8bc348ebc2d
msalsas <manolez@gmail.com>
2019-07-23 21:16:45 +0200

    Add migration

---
+++ src/Migrations/Version20190723191557.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190723191557 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE currency_rate (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, currency VARCHAR(3) NOT NULL, rate_to_eur DOUBLE PRECISION NOT NULL, symbol VARCHAR(5) NOT NULL)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE currency_rate');
+    }
+}
